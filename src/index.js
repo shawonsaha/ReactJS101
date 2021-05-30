9b7c58ab9475928bb89d54afec5ef8b59070c443
Shawon Saha <shawondroid@gmail.com>
2021-05-30 12:49:26 +0600

    Props Hands Dirty

--- src/index.js
+++ src/index.js
@@ -3,27 +3,28 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
-//This is main fucntion generally called App()
-//Where all other microcomponent will placed
+const title = "Atomic Habits: Easy & Proven Way";
+const author = "James Clear";
+const image =
+  "https://images-na.ssl-images-amazon.com/images/I/713mzPe9SwS._AC_UL200_SR200,200_.jpg";
+//This is main fucntion generally called App() Where all other microcomponent will placed
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
+      <Book haha="kire" />
+      <Book job="devoloper" />
       <Book />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/713mzPe9SwS._AC_UL200_SR200,200_.jpg"
-        alt="Atomic Habits"
-      />
-      <h4>Atomic Habits: Easy & Proven Way</h4>
-      <h5>Shawon Saha</h5>
+      <img src={image} alt={title} />
+      <h4>{title}</h4>
+      <h5>{author}</h5>
     </article>
   );
 };
