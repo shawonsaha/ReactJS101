7cde68668f713d75ff1bb64829453fb6c1155c10
Shawon Saha <shawondroid@gmail.com>
2021-06-01 15:17:12 +0600

    Proper List

--- src/index.js
+++ src/index.js
@@ -2,50 +2,46 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
-//creating book object
-// const firstBook = {
-//   image:
-//     "https://images-na.ssl-images-amazon.com/images/I/713mzPe9SwS._AC_UL200_SR200,200_.jpg",
-//   title: "Atomic Habits: Easy & Proven Way",
-//   author: "James Clear"
-// };
 
-// const secondBook = {
-//   image:
-//     "https://images-na.ssl-images-amazon.com/images/I/91g%2BaK0nCnL._AC_UL200_SR200,200_.jpg",
-//   title: "The Four Winds: A Novel",
-//   author: "Kristin Hannah"
-// };
+const books = [
+  {
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/713mzPe9SwS._AC_UL200_SR200,200_.jpg",
+    title: "Atomic Habits: Easy & Proven Way",
+    author: "James Clear"
+  },
+  {
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/91g%2BaK0nCnL._AC_UL200_SR200,200_.jpg",
+    title: "The Four Winds: A Novel",
+    author: "Kristin Hannah"
+  },
+  {
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/91g%2BaK0nCnL._AC_UL200_SR200,200_.jpg",
+    title: "The Four Winds: A Novel",
+    author: "Kristin Hannah"
+  }
+];
 
-//This is main fucntion generally called App() Where all other microcomponent will placed
-const names = ["suvra", "saurav", "shawon"];
-const newNames = names.map((name) => {
-  return <h1>{name}</h1>;
-});
 function BookList() {
   return (
     <section className="booklist">
-      {newNames}
-      {/* <Book
-        image={firstBook.image}
-        title={firstBook.title}
-        author={firstBook.author}>
-        <p>
-          sodales neque sodales ut etiam sit amet nisl purus in mollis nunc sed
-          id semper risus in hendrerit gravida rutrum
-        </p>
-      </Book> */}
+      {books.map((book) => {
+        return <Book book={book} />;
+      })}
     </section>
   );
 }
 
-const Book = ({ image, title, author, children }) => {
+const Book = (props) => {
+  const { image, title, author } = props.book;
+  console.log(props.book);
   return (
     <article className="book">
       <img src={image} alt={title} />
       <h4>{title}</h4>
       <h5>{author}</h5>
-      {children}
     </article>
   );
 };
