1514cdf79c6ca6a012b6ffe0a32a8da0fc9aa8eb
Shawon Saha <shawondroid@gmail.com>
2021-06-02 21:53:53 +0600

    Key Prop and Spread Operator

--- src/index.js
+++ src/index.js
@@ -5,22 +5,25 @@ import "./index.css";
 
 const books = [
   {
+    id: 1,
     image:
       "https://images-na.ssl-images-amazon.com/images/I/713mzPe9SwS._AC_UL200_SR200,200_.jpg",
     title: "Atomic Habits: Easy & Proven Way",
     author: "James Clear"
   },
   {
+    id: 2,
     image:
       "https://images-na.ssl-images-amazon.com/images/I/91g%2BaK0nCnL._AC_UL200_SR200,200_.jpg",
     title: "The Four Winds: A Novel",
     author: "Kristin Hannah"
   },
   {
+    id: 3,
     image:
-      "https://images-na.ssl-images-amazon.com/images/I/91g%2BaK0nCnL._AC_UL200_SR200,200_.jpg",
-    title: "The Four Winds: A Novel",
-    author: "Kristin Hannah"
+      "https://images-na.ssl-images-amazon.com/images/I/713XUMrojkL._AC_UL200_SR200,200_.jpg",
+    title: "What Happened to You?: Conversations on Trauma",
+    author: "Oprah Winfrey"
   }
 ];
 
@@ -28,15 +31,14 @@ function BookList() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book book={book} />;
+        return <Book key={book.id} {...book} />;
       })}
     </section>
   );
 }
 
-const Book = (props) => {
-  const { image, title, author } = props.book;
-  console.log(props.book);
+const Book = ({ image, title, author }) => {
+  console.log(image, title, author);
   return (
     <article className="book">
       <img src={image} alt={title} />
