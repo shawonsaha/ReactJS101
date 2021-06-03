73f07fdb2e2ce342519d1a3fab63d3529a24fb0d
Shawon Saha <shawondroid@gmail.com>
2021-06-03 15:38:07 +0600

    Import and Export Modules Building react application moduler

--- src/index.js
+++ src/index.js
@@ -3,29 +3,8 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
-const books = [
-  {
-    id: 1,
-    image:
-      "https://images-na.ssl-images-amazon.com/images/I/713mzPe9SwS._AC_UL200_SR200,200_.jpg",
-    title: "Atomic Habits: Easy & Proven Way",
-    author: "James Clear"
-  },
-  {
-    id: 2,
-    image:
-      "https://images-na.ssl-images-amazon.com/images/I/91g%2BaK0nCnL._AC_UL200_SR200,200_.jpg",
-    title: "The Four Winds: A Novel",
-    author: "Kristin Hannah"
-  },
-  {
-    id: 3,
-    image:
-      "https://images-na.ssl-images-amazon.com/images/I/713XUMrojkL._AC_UL200_SR200,200_.jpg",
-    title: "What Happened to You?: Conversations on Trauma",
-    author: "Oprah Winfrey"
-  }
-];
+import { books } from "./books";
+import Book from "./Book";
 
 function BookList() {
   return (
@@ -36,16 +15,5 @@ function BookList() {
     </section>
   );
 }
-
-const Book = ({ image, title, author }) => {
-  console.log(image, title, author);
-  return (
-    <article className="book">
-      <img src={image} alt={title} />
-      <h4>{title}</h4>
-      <h5>{author}</h5>
-    </article>
-  );
-};
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
